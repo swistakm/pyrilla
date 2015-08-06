caada6ed51e4c261dc7460568a0dcb9fefc201a1
mjaworski <mjaworski@opera.com>
2015-08-06 18:05:26 +0200

    project bootstrapping

---
+++ setup.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import os
+from distutils.core import setup, Extension
+
+from Cython.Distutils import build_ext
+
+EXTENSIONS_INCLUDE_DIRS = ["gorilla-audio/include"]
+EXTENSIONS_EXTRA_OBJECTS = [
+    "gorilla-audio/bin/osx/Release/libgorilla.a",
+    "/System/Library/Frameworks/OpenAL.framework/OpenAL",
+]
+
+
+def get_version(version_tuple):
+    if not isinstance(version_tuple[-1], int):
+        return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
+    return '.'.join(map(str, version_tuple))
+
+
+try:
+    from pypandoc import convert
+
+    def read_md(f):
+        return convert(f, 'rst')
+except ImportError:
+    convert = None
+    print(
+        "warning: pypandoc module not found, could not convert Markdown to RST"
+    )
+
+    def read_md(f):
+        return open(f, 'r').read()  # noqa
+
+
+init = os.path.join(os.path.dirname(__file__), 'pyrilla', '__init__.py')
+version_line = list(filter(lambda l: l.startswith('VERSION'), open(init)))[0]
+
+VERSION = get_version(eval(version_line.split('=')[-1]))
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+
+
+setup(
+    name='pyrilla',
+    version=VERSION,
+    packages=['pyrilla'],
+
+    author='Michał Jaworski',
+    author_email='swistakm@gmail.com',
+
+    description="Python binding to gorilla-audio library",
+    long_description=read_md(README),
+    url="https://github.com/swistakm/pyrilla",
+
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[
+        Extension(
+            "pyrilla.ga", ["extensions/ga.pyx"],
+            include_dirs=EXTENSIONS_INCLUDE_DIRS,
+            extra_objects=EXTENSIONS_INCLUDE_DIRS,
+        ),
+        Extension(
+            "pyrilla.gau", ["extensions/gau.pyx"],
+            include_dirs=EXTENSIONS_INCLUDE_DIRS,
+            extra_objects=EXTENSIONS_INCLUDE_DIRS,
+        ),
+        Extension(
+            "pyrilla.gc", ["extensions/gc.pyx"],
+            include_dirs=EXTENSIONS_INCLUDE_DIRS,
+            extra_objects=EXTENSIONS_INCLUDE_DIRS,
+        )
+    ],
+
+    setup_requires=['cython'],
+    install_requires=[],
+
+    include_package_data=True,
+
+    license='BSD',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+    ],
+)
