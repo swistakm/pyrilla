c4164232eb8ba530580620d16596b73c6bb6c717
mjaworski <mjaworski@opera.com>
2016-10-28 16:42:26 +0200

    ci: add dockerfiles for linux builds

--- ci/docker_build_wheels.sh
+++ ci/docker_build_wheels.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env bash
 
-mkdir /opt/cmake
-cd /opt/cmake
+# note: something with cmake is broken so we need to run it manually
+# todo: rectify this
+cd /io/gorilla-audio/ext/libogg
+cmake .
 
-# todo: try to create own docker images based on quay.io/pypa/manylinux1_*
-# see: https://github.com/andystanton/docker-gcc-cmake-gl/blob/master/Dockerfile
-# see: https://quay.io/repository/pypa/manylinux1_i686
-curl -O https://cmake.org/files/v3.6/cmake-3.6.2.tar.gz && tar -xvf cmake-3.6.2.tar.gz
-cd cmake-3.6.2
-./bootstrap
-make
-make install
-
-#yum install -y cmake
+# note: could not do that in Dockerfile
 yum install -y openal-devel
-#yum install -y libogg-devel
 
-mkdir /tmp/cmake-build/
-cd /tmp/cmake-build
-
+cd /io/
 cmake /io/gorilla-audio/build
-cmake --build --config Release .
+cmake --build .
 
 for PYBIN in /opt/python/*/bin; do
     # note: latest version of cython does not support py26 so make sure we
