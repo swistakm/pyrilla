4378b1bb4764311df83a79156538feda0b26956a
mjaworski <mjaworski@opera.com>
2016-10-28 15:13:01 +0200

    ci: install latest stable cmake in container [skip appveyor]

--- ci/docker_build_wheels.sh
+++ ci/docker_build_wheels.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env bash
 
-yum install -y cmake
+mkdir /opt/cmake
+cd /opt/cmake
+
+# todo: try to create own docker images based on quay.io/pypa/manylinux1_*
+# see: https://github.com/andystanton/docker-gcc-cmake-gl/blob/master/Dockerfile
+# see: https://quay.io/repository/pypa/manylinux1_i686
+curl -O https://cmake.org/files/v3.6/cmake-3.6.2.tar.gz && tar -xvf cmake-3.6.2.tar.gz
+cd cmake-3.6.2
+./bootstrap
+make
+make install
+
+#yum install -y cmake
+yum install -y openal-devel
+#yum install -y libogg-devel
+
+mkdir /tmp/cmake-build/
+cd /tmp/cmake-build
+
 cmake /io/gorilla-audio/build
-cmake --build . --config Release
+cmake --build --config Release .
 
 for PYBIN in /opt/python/*/bin; do
     # note: latest version of cython does not support py26 so make sure we
@@ -10,7 +28,7 @@ for PYBIN in /opt/python/*/bin; do
     if [[ ! $PYBIN == *"26"* ]]; then
         echo -e "\n\nBuilding wheel for $PYBIN"
 
-        ${PYBIN}/pip install
+        ${PYBIN}/pip install Cython
         ${PYBIN}/pip wheel /io/ -w /io/dist/
 
     fi
