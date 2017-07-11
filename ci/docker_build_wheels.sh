7fb225faea3c9401ef914f232a65fb365feac10f
mjaworski <mjaworski@opera.com>
2017-07-11 09:18:34 +0200

    add travis fold in docker build script

--- ci/docker_build_wheels.sh
+++ ci/docker_build_wheels.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
+travis_fold() {
+  local action=$1
+  local name=$2
+  echo -en "travis_fold:${action}:${name}\r"
+}
 
+
+travis_fold start cmake-libogg
 # note: something with cmake is broken so we need to run it manually
 # todo: rectify this
 cd /io/gorilla-audio/ext/libogg
 cmake .
+travis_fold end cmake-libogg
 
+
+travis_fold start yum-openal
 # note: could not do that in Dockerfile
 # todo: try to support it directly in gorilla-audio so we don't have to
 # todo: install system package for headers
 yum install -y openal-devel
+travis_fold end yum-openal
+
 
 cd /io/
 # note: we on x86_64 we need to excplicitely enable Position Independent Code
@@ -23,9 +35,10 @@ for PY in /opt/python/*/bin/python; do echo "* $(${PY} --version 2>&1)"; done
 for PYBIN in /opt/python/*/bin; do
     echo -e "\n\nBuilding wheel for $(${PYBIN}/python --version 2>&1)"
 
+    travis_fold start wheel-${PYBIN}
     ${PYBIN}/pip install Cython
     ${PYBIN}/pip wheel /io/ -w /io/dist-wip/
-
+    travis_fold end wheel-${PYBIN}
 done
 
 # Bundle external shared libraries into the wheels and fix platform tags
